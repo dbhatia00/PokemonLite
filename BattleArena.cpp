00e8ee7aaac6a0b6508c4cfbee4be8e5ffa200b4
Dev B <devb@bu.edu>
2019-12-09 17:49:04 -0500

    Added pokemon updates, rival, BA

---
+++ BattleArena.cpp
@@ -0,0 +1,74 @@
+#include "BattleArena.h"
+
+    BattleArena::BattleArena(){
+        display_code = 'A';
+        pokemon_count = 0;
+        max_num_rivals = 3;
+        num_rivals_remaining = max_num_rivals;
+        dollar_cost_per_fight = 4;
+        stamina_cost_per_fight = 3;
+        state = RIVALS_AVAILABLE;
+        cout << "Battle Arena default constructed." << endl;
+    }
+
+    BattleArena::BattleArena(unsigned int max_rivals, unsigned int stamina_cost, double dollar_cost, int in_id, Point2D in_loc):Building('A',in_id,in_loc){
+        max_num_rivals = max_rivals;
+        num_rivals_remaining = max_num_rivals;
+        pokemon_count =0;
+        dollar_cost_per_fight = 4;
+        stamina_cost_per_fight = 3;
+        state = RIVALS_AVAILABLE;
+        cout << "BattleArena constructed." << endl;
+    }
+    unsigned int BattleArena::GetNumRivalsRemaining(){
+        return num_rivals_remaining;
+    }
+    bool BattleArena::HasEnoughRivals(){
+        if (num_rivals_remaining > 0 ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    double BattleArena::GetDollarCost(){
+        return dollar_cost_per_fight;
+    }
+    unsigned int BattleArena::GetStaminaCost(){
+        return stamina_cost_per_fight;
+    }
+    bool BattleArena::IsAbleToFight(double budget, unsigned int stamina){
+        if (budget>=dollar_cost_per_fight && stamina>=stamina_cost_per_fight){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool BattleArena::Update(){
+        if(state !=NO_RIVALS_AVAILABLE && num_rivals_remaining == 0){
+            state = NO_RIVALS_AVAILABLE;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool BattleArena::IsBeaten(){
+        if (num_rivals_remaining == 0){
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
+    }
+    void BattleArena::ShowStatus(){
+        cout << "Battle Arena status: " << endl;
+        Building::ShowStatus();
+        cout << "\tMax number of rivals: " << max_num_rivals << endl;
+        cout << "\tStamina cost per fight: " << stamina_cost_per_fight << endl;
+        cout << "\tPokemon dollars per fight: " << dollar_cost_per_fight<< endl;
+        cout << "\t"<<num_rivals_remaining << " rival(s) are reminaing for this arena."<< endl;
+    }
--- Pokemon.h
+++ Pokemon.h
@@ -1,5 +1,6 @@
 #include "PokemonCenter.h"
 #include "PokemonGym.h"
+#include "Rival.h"
 #include <iostream>
 #include <math.h>
 #include <time.h>
@@ -12,6 +13,8 @@ class Pokemon : public GameObject{
         Pokemon();
         Pokemon(char);
         Pokemon(string, int, char, unsigned int, Point2D);
+	    Pokemon(string in_name, double speed, double hp, double phys_dmg, double
+		magic_dmg, double def, int in_id, char in_code, Point2D in_loc);
         virtual ~Pokemon();
         void StartMoving(Point2D);
         void StartMovingToCenter(PokemonCenter*);
@@ -24,11 +27,24 @@ class Pokemon : public GameObject{
         void ShowStatus();
         bool Update();
         string GetName();
-
+        //pa4 additions ->
+        bool IsAlive();
+        void TakeHit(double, double, double);
+        void ReadyBattle(Rival*);
+        bool StartBattle();
+        void StartMovingToArena(BattleArena* arena);
     protected:
         bool UpdateLocation();
         void SetupDestination(Point2D);
-
+        //pa4 additions ->
+	    double health = 20;
+	    double store_health = health;
+	    double physical_damage = 5;
+	    double magical_damage = 4;
+	    double defense;
+	    Rival* target;
+	    bool is_in_arena;
+	    BattleArena * current_arena;
     private:
         double speed;
         bool is_in_center = 0;
@@ -53,7 +69,12 @@ enum PokemonStates {
         MOVING_TO_GYM = 5,
         MOVING_TO_CENTER = 6,
         TRAINING_IN_GYM = 7,
-        RECOVERING_STAMINA = 8
+        RECOVERING_STAMINA = 8,
+        //pa4 additions ->
+	IN_ARENA = 9,
+	MOVING_TO_ARENA = 10,
+	BATTLE = 11,
+	FAINTED = 12
     };
 static double GetRandomAmountOfPokemonDollars(){
     return rand()%2;
